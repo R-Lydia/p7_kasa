caaa19876db154329f52eeb5128d4f2cf25d866b
Lydia <lydiarugby@gmail.com>
2023-02-06 16:59:22 +0100

    création pages Apropos et 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import '../styles/pages/404.css';
 
 export default function Error() {
     document.title = 'Kasa - Page introuvable';
     return (
-        <div>
-            <h1>Erreur 404</h1>
-        </div>
-    );
+        <main className='main_error'>
+            <h1 className='error_title'>404</h1>
+            <p className='error_text'>Oups! La page que <br/> vous demandez n'existe pas.</p>
+            <Link className='error_backhome' to='/'>Retourner sur la page d'accueil</Link>
+        </main>
+    )
 };
--- src/pages/Apropos.js
+++ src/pages/Apropos.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import Banner from '../components/Banner';
+import Collapse from '../components/Collapse';
+import bannerApropos from '../assets/banner_apropos.png';
 
 export default function Apropos() {
     document.title = 'Kasa - A propos';
     return (
-        <div>
-            <h1>A propos de Kasa</h1>
-        </div>
+        <main className='main_apropos'>
+            <Banner img={bannerApropos} />
+            <Collapse title="Fiabilité" description="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes."/>
+            <Collapse title="Respect" description="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."/>
+            <Collapse title="Service" description="Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question."/>
+            <Collapse title="Sécurité" description="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."/>
+        </main>
     );
 };
